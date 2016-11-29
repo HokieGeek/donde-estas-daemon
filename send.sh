e35cca89c3f733d0fab38c61fa33e0f86de1c1ef
HokieGeek <andres.f.perez@gmail.com>
2016-11-29 15:24:42 -0500

    Updated tester

--- send.sh
+++ send.sh
@@ -14,4 +14,18 @@ curl -v `hostname --fqdn`:8585/update --data \
 	"whitelist" : [],
 	"following" : []
 }'
-curl -v `hostname --fqdn`:8585/person --data '{"id":42}'
+curl -v `hostname --fqdn`:8585/update --data \
+'{
+    "id" : 99,
+    "name" : "raboof",
+	"position": {
+		"tov": "2009-11-10T23:00:00Z",
+		"latitude": 0.0,
+		"longitude": 0.0,
+		"elevation": 0.0
+	},
+	"visible" : true,
+	"whitelist" : [],
+	"following" : []
+}'
+curl -v `hostname --fqdn`:8585/person --data '{"ids":[42,99]}'
