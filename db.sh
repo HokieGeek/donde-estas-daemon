4de8700929dd180dd544779083e9928d21881b78
HokieGeek <andres.f.perez@gmail.com>
2016-11-29 12:51:16 -0500

    Cleaned up the db.sh script

--- db.sh
+++ db.sh
@@ -7,9 +7,3 @@ if [[ $(docker ps --quiet --filter="name=${name}") == "" ]]; then
     # docker run --detach --volume /usr/local/var/lib/couchdb:/usr/local/var/lib/couchdb --name dondedb --env COUCHDB_USER=donde --env COUCHDB_PASSWORD=dondepwd couchdb
     docker run --detach --volume /usr/local/var/lib/couchdb:/usr/local/var/lib/couchdb --name ${name} couchdb
 fi
-
-# cmd=$1; shift
-
-# set -x
-# docker run -it --rm --link ${name}:db hokiegeek/alpine-curl -v -X ${cmd:-"GET"} http://db:5984/$@
-# docker run -it --rm --link ${name}:db hokiegeek/alpine-curl $@
