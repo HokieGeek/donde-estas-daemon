473b9a961f9372a398b5249de8857168d5ef74f7
HokieGeek <andres.f.perez@gmail.com>
2016-11-29 00:01:08 -0500

    Updated test harnesses

--- db.sh
+++ db.sh
@@ -4,12 +4,12 @@ name="dondedb"
 
 if [[ $(docker ps --quiet --filter="name=${name}") == "" ]]; then
     echo "Creating an instance of the database"
-    # docker run --detach --name ${name} --env COUCHDB_USER=admin --env COUCHDB_PASSWORD=password couchdb
-    docker run --detach --name ${name} couchdb
+    # docker run --detach --volume /usr/local/var/lib/couchdb:/usr/local/var/lib/couchdb --name dondedb --env COUCHDB_USER=donde --env COUCHDB_PASSWORD=dondepwd couchdb
+    docker run --detach --volume /usr/local/var/lib/couchdb:/usr/local/var/lib/couchdb --name ${name} couchdb
 fi
 
-cmd=$1; shift
+# cmd=$1; shift
 
 # set -x
-docker run -it --rm --link ${name}:db hokiegeek/alpine-curl -v -X ${cmd:-"GET"} http://db:5984/$@
+# docker run -it --rm --link ${name}:db hokiegeek/alpine-curl -v -X ${cmd:-"GET"} http://db:5984/$@
 # docker run -it --rm --link ${name}:db hokiegeek/alpine-curl $@
--- send.sh
+++ send.sh
@@ -1,3 +1,17 @@
 #!/bin/sh
 
+curl -v `hostname --fqdn`:8585/update --data \
+'{
+    "id" : 42,
+    "name" : "foobar",
+	"position": {
+		"tov": "2009-11-10T23:00:00Z",
+		"latitude": 0.0,
+		"longitude": 0.0,
+		"elevation": 0.0
+	},
+	"visible" : true,
+	"whitelist" : [],
+	"following" : []
+}'
 curl -v `hostname --fqdn`:8585/person --data '{"id":42}'
