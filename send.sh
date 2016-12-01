727d08efe45ddc22aebfab302775a7310a16569d
HokieGeek <andres.f.perez@gmail.com>
2016-12-01 15:33:17 -0500

    Tweaked the test data a tad

--- send.sh
+++ send.sh
@@ -6,12 +6,12 @@ curl -v `hostname --fqdn`:8585/update --data \
     "name" : "foobar",
 	"position": {
 		"tov": "2009-11-10T23:00:00Z",
-		"latitude": 0.0,
-		"longitude": 0.0,
-		"elevation": 0.0
+		"latitude": 0.1,
+		"longitude": 0.2,
+		"elevation": 0.3
 	},
 	"visible" : true,
-	"whitelist" : [],
+	"whitelist" : ["99"],
 	"following" : []
 }'
 curl -v `hostname --fqdn`:8585/update --data \
@@ -19,13 +19,13 @@ curl -v `hostname --fqdn`:8585/update --data \
     "id" : "99",
     "name" : "raboof",
 	"position": {
-		"tov": "2009-11-10T23:00:00Z",
-		"latitude": 0.0,
-		"longitude": 0.0,
-		"elevation": 0.0
+		"tov": "2009-11-10T22:00:00Z",
+		"latitude": 0.4,
+		"longitude": 0.5,
+		"elevation": 0.6
 	},
 	"visible" : true,
-	"whitelist" : [],
-	"following" : []
+	"whitelist" : ["42"],
+	"following" : ["42"]
 }'
 curl -v `hostname --fqdn`:8585/person --data '{"ids":["42","99"]}'
