c7d87c3044e5d2b37fa37709b76a8a10c4663c9e
HokieGeek <andres.f.perez@gmail.com>
2016-12-01 17:41:42 -0500

    Minor update to the db script

--- db.sh
+++ db.sh
@@ -2,8 +2,14 @@
 
 name="dondedb"
 
-if [[ $(docker ps --quiet --filter="name=${name}") == "" ]]; then
-    echo "Creating an instance of the database"
-    # docker run --detach --volume /usr/local/var/lib/couchdb:/usr/local/var/lib/couchdb --name dondedb --env COUCHDB_USER=donde --env COUCHDB_PASSWORD=dondepwd couchdb
-    docker run --detach --volume /usr/local/var/lib/couchdb:/usr/local/var/lib/couchdb --name ${name} couchdb
+if [[ $1 == "--kill" ]]; then
+    docker stop ${name}
+    docker kill ${name}
+    docker rm ${name}
+else
+    if [[ $(docker ps --quiet --filter="name=${name}") == "" ]]; then
+        echo "Creating an instance of the database"
+        # docker run --detach --volume /usr/local/var/lib/couchdb:/usr/local/var/lib/couchdb --name dondedb --env COUCHDB_USER=donde --env COUCHDB_PASSWORD=dondepwd couchdb
+        docker run --detach --volume /usr/local/var/lib/couchdb:/usr/local/var/lib/couchdb --name ${name} couchdb
+    fi
 fi
