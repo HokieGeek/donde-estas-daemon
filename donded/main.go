cd0f665319f66e40b2b579c27a52404be8555b44
HokieGeek <andres.f.perez@gmail.com>
2016-12-01 21:32:34 -0500

    Added one more option

--- donded/main.go
+++ donded/main.go
@@ -5,16 +5,24 @@ import (
 	"github.com/hokiegeek/donde-estas-daemon"
 	"log"
 	"os"
+	"strconv"
+	"strings"
 )
 
 func main() {
-	httpPortPtr := flag.Int("port", 8585, "Specify the port to use")
+	httpPortPtr := flag.Int("port", 8080, "Specify the port to use")
+	databaseUrlPtr := flag.String("dburl", "db:5984", "The hostname[:port] of the database")
 	flag.Parse()
 
+	sepPos := strings.LastIndex(*databaseUrlPtr, ":")
+	dbHost := (*databaseUrlPtr)[:sepPos]
+	dbPort, _ := strconv.Atoi((*databaseUrlPtr)[sepPos+1:])
+
 	logger := log.New(os.Stdout, "", 0)
+	logger.Printf("Connecting to %s on port %d\n", dbHost, dbPort)
 	logger.Printf("Serving on port %d\n", *httpPortPtr)
 
-	params := dondeestas.DbClientParams{dondeestas.CouchDB, "donde", "db", 5984}
+	params := dondeestas.DbClientParams{dondeestas.CouchDB, "donde", dbHost, dbPort}
 
 	db, err := dondeestas.NewDbClient(params)
 	if err != nil {
