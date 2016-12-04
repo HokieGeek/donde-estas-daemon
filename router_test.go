c86241f76597247f6b445ad915f00284604602ba
HokieGeek <andres.f.perez@gmail.com>
2016-12-03 21:34:41 -0500

    Much more complete testing of the router file

--- router_test.go
+++ router_test.go
@@ -3,14 +3,22 @@ package dondeestas
 import (
 	"bytes"
 	"encoding/json"
-	"fmt"
-	// "io/ioutil"
+	"log"
+	"math/rand"
 	"net/http"
 	"net/http/httptest"
+	"os"
 	"testing"
-	// "math/rand"
-	// "log"
 )
+
+func getExpectedPersonRequest() (*http.Request, *Person, *httptest.ResponseRecorder) {
+	expectedPerson, _ := createRandomPerson()
+	expectedPersonJson, _ := json.Marshal(expectedPerson)
+	expectedPersonStr := string(expectedPersonJson)
+	req := httptest.NewRequest("GET", "http://"+createRandomString(), bytes.NewBufferString(expectedPersonStr))
+
+	return req, expectedPerson, httptest.NewRecorder()
+}
 
 func TestRouting_GetJson(t *testing.T) {
 	expectedPerson, _ := createRandomPerson()
@@ -34,54 +42,88 @@ func TestRouting_PostJson(t *testing.T) {
 		t.Fatalf("Expected http status %d but found %d", expectedStatus, response.Code)
 	}
 
-	fmt.Print("'")
-	fmt.Print(response.Body)
-	fmt.Println("'")
 	var person Person
 	if err := json.Unmarshal(response.Body.Bytes(), &person); err != nil {
 		t.Fatalf("Encountered error when retrieving json from string: %s", err)
 	} else if !arePersonEqual(expectedPerson, &person) {
 		t.Fatal("Did not receive expected person")
 	}
-	/*
-	if err := getJson(ioutil.NopCloser(response.Body), person); err != nil {
-		t.Fatalf("Encountered error when retrieving json from string: %s", err)
-		// TODO: } else if !arePersonEqual(expectedPerson, &person) {
-		// t.Fatal("Did not receive expected person")
-	}*/
+	// TODO: more?
 }
-/*
+
 func TestRouting_UpdatePersonHandler(t *testing.T) {
 	log := log.New(os.Stdout, "", 0)
 	response := httptest.NewRecorder()
 
 	expectedPerson, _ := createRandomPerson()
-    req := httptest.NewRequest("GET", createRandomString(), bytes.NewBufferString(expectedPersonStr))
+	expectedPersonJson, _ := json.Marshal(expectedPerson)
+	expectedPersonStr := string(expectedPersonJson)
+	req := httptest.NewRequest("GET", "http://"+createRandomString(), bytes.NewBufferString(expectedPersonStr))
 
 	db, server, _ := createRandomDbClient()
 	defer server.Close()
 
 	UpdatePersonHandler(log, db, response, req)
+
+	if response.Code != http.StatusCreated {
+		t.Errorf("Did not get expected HTTP status code. Instead got: %d", response.Code)
+	}
+
+	if person, err := (*db).Get(expectedPerson.Id); err != nil {
+		t.Fatalf("Encountered error when retrieving person: %s", err)
+	} else if !arePersonEqual(expectedPerson, person) {
+		t.Fatal("Retrieved Person is not equivalent to the expected Person")
+	}
+
+	// TODO: more?
 }
 
 func TestRouting_PersonRequestHandler(t *testing.T) {
 	log := log.New(os.Stdout, "", 0)
 	response := httptest.NewRecorder()
 
-    req := httptest.NewRequest("GET", createRandomString(), bytes.NewBufferString(expectedPersonStr))
+	expectedPerson, _ := createRandomPerson()
+
+	var dataReq PersonDataRequest
+	dataReq.Ids = make([]string, 1)
+	dataReq.Ids[0] = expectedPerson.Id
+	personDataRequestJson, _ := json.Marshal(dataReq)
+	personDataRequestStr := string(personDataRequestJson)
+	req := httptest.NewRequest("GET", "http://"+createRandomString(), bytes.NewBufferString(personDataRequestStr))
 
 	db, server, _ := createRandomDbClient()
 	defer server.Close()
 
+	(*db).Create(*expectedPerson)
+
 	PersonRequestHandler(log, db, response, req)
+
+	// TODO: check status code
+	// TODO: check number of people returned
+
+	var person PersonDataResponse
+	if err := json.Unmarshal(response.Body.Bytes(), &person); err != nil {
+		t.Fatalf("Encountered error when retrieving json from string: %s", err)
+	} else if !arePersonEqual(expectedPerson, &person.People[0]) {
+		t.Fatal("Did not receive expected person")
+	}
+
+	// TODO: more?
+	/*
+		dataReq.Ids = append(dataReq.Ids, createRandomString())
+		personDataRequestJson, _ = json.Marshal(dataReq)
+		personDataRequestStr = string(personDataRequestJson)
+		req = httptest.NewRequest("GET", "http://"+createRandomString(), bytes.NewBufferString(personDataRequestStr))
+	*/
 }
 
-func TestRouting_New(t *testing.T) { // TODO TEST FOR BAD INPUT VALUES
+func TestRouting_ListenAndServe(t *testing.T) {
+	t.Skip("TODO")
 	port := rand.Int()
 	log := log.New(os.Stdout, "", 0)
 	db, server, _ := createRandomDbClient()
 	defer server.Close()
 
-	New(log *log.Logger, port int, db *dbclient)
+	// TODO: use a goroutine to do some tests
+	go ListenAndServe(log, port, db)
 }
-*/
