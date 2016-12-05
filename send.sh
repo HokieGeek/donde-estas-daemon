78328d8bd17bd6a1b9f78fedec6978beeaaf2f4a
HokieGeek <andres.f.perez@gmail.com>
2016-12-04 22:17:14 -0500

    Added to my simple little integration test

--- send.sh
+++ send.sh
@@ -5,10 +5,10 @@ curl -v `hostname --fqdn`:8585/update --data \
     "id" : "42",
     "name" : "foobar",
     "position": {
-        "tov": "2009-11-10T23:00:00Z",
-        "latitude": 0.1,
-        "longitude": 0.2,
-        "elevation": 0.3
+        "tov": "2009-11-10T23:00:00.210Z",
+        "latitude": 0,
+        "longitude": 0,
+        "elevation": 0
     },
     "visible" : true,
     "whitelist" : ["99"],
@@ -28,4 +28,22 @@ curl -v `hostname --fqdn`:8585/update --data \
     "whitelist" : ["42"],
     "following" : ["42"]
 }'
+
 curl -v `hostname --fqdn`:8585/person --data '{"ids":["42","99"]}'
+
+curl -v `hostname --fqdn`:8585/update --data \
+'{
+    "id" : "42",
+    "name" : "foobar",
+    "position": {
+        "tov": "2009-11-10T23:00:00.210Z",
+        "latitude": '$((RANDOM))',
+        "longitude": '$((RANDOM))',
+        "elevation": '$((RANDOM))'
+    },
+    "visible" : true,
+    "whitelist" : ["99"],
+    "following" : []
+}'
+
+curl -v `hostname --fqdn`:8585/person --data '{"ids":["42"]}'
