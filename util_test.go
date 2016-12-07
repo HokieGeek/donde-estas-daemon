7cbfcde976e0887ad18aeb6159106a76c11378a2
HokieGeek <andres.f.perez@gmail.com>
2016-12-07 11:01:08 -0500

    Renaming the function because...

--- util_test.go
+++ util_test.go
@@ -12,14 +12,14 @@ func stringToReadCloser(str string) io.ReadCloser {
 	return ioutil.NopCloser(bytes.NewReader(bytes.NewBufferString(str).Bytes()))
 }
 
-func TestReadCloserToJson(t *testing.T) {
+func TestReadCloserJsonToStruct(t *testing.T) {
     // TODO: make this just a JSON object, not a Person
 	expectedPerson, _ := createRandomPerson()
 	expectedPersonJson, _ := json.Marshal(expectedPerson)
 	expectedPersonStr := string(expectedPersonJson)
 
 	var person Person
-	if err := ReadCloserJsonToStruct(stringToReadCloser(expectedPersonStr), person); err != nil {
+	if err := readCloserJsonToStruct(stringToReadCloser(expectedPersonStr), person); err != nil {
 		t.Fatalf("Encountered error when retrieving json from string: %s", err)
 	}
 	
@@ -31,12 +31,12 @@ func TestReadCloserToJson(t *testing.T) {
 	if err := req.Body.Close(); err != nil {
 		t.Fatalf("Could not close test request body!")
 	}
-	if err := ReadCloserJsonToStruct(req.Body, person); err == nil {
+	if err := readCloserJsonToStruct(req.Body, person); err == nil {
 		t.Error("Did not receive expected error when reading closed stream")
 	}
 	
 	// Incorrect JSON object
-	if err := ReadCloserJsonToStruct(ioutil.NopCloser(bytes.NewReader(bytes.NewBufferString(`{"id":"foo"}`).Bytes())), person); err == nil  {
+	if err := readCloserJsonToStruct(ioutil.NopCloser(bytes.NewReader(bytes.NewBufferString(`{"id":"foo"}`).Bytes())), person); err == nil  {
 		t.Error("Did not receive expected error on bad JSON unmarshalling")
 	}
     */
