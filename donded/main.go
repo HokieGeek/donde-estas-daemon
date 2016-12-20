d847ac32d9f5ffb354dd4c8fec62f93f533b09d0
HokieGeek <andres.f.perez@gmail.com>
2016-12-20 15:19:19 -0500

    Cleaning up the main method

--- donded/main.go
+++ donded/main.go
@@ -2,16 +2,18 @@ package main
 
 import (
 	"flag"
-	"github.com/hokiegeek/donde-estas-daemon"
 	"log"
 	"os"
 	"strconv"
 	"strings"
+
+	"github.com/hokiegeek/donde-estas-daemon"
 )
 
 func main() {
-	httpPortPtr := flag.Int("port", 8080, "Specify the port to use")
-	databaseURLPtr := flag.String("dburl", "db:5984", "The hostname[:port] of the database")
+	httpPortPtr := flag.Int("port", 8080, "Specify the port to use for the HTTP server")
+	databaseURLPtr := flag.String("dburl", "db:5984", "The hostname:port of the database server")
+	databaseNamePtr := flag.String("dbname", "donde", "The name of the database")
 	flag.Parse()
 
 	sepPos := strings.LastIndex(*databaseURLPtr, ":")
@@ -19,10 +21,10 @@ func main() {
 	dbPort, _ := strconv.Atoi((*databaseURLPtr)[sepPos+1:])
 
 	logger := log.New(os.Stdout, "", 0)
-	logger.Printf("Connecting to %s on port %d\n", dbHost, dbPort)
-	logger.Printf("Serving on port %d\n", *httpPortPtr)
+	logger.Printf(":: Connecting to database %s at %s on port %d\n", dbHost, dbPort)
+	logger.Printf(":: Serving on port %d\n", *httpPortPtr)
 
-	params := dondeestas.DbClientParams{dondeestas.CouchDB, "donde", dbHost, dbPort}
+	params := dondeestas.DbClientParams{dondeestas.CouchDB, *databaseNamePtr, dbHost, dbPort}
 
 	db, err := dondeestas.NewDbClient(params)
 	if err != nil {
